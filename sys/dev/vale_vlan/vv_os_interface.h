597cddda7adb5614bd54c73080fc81a507d43333
Stefano Duo <duostefano93@gmail.com>
2018-06-21 14:55:31 +0200

    linked list arguments nomenclature fixed

--- sys/dev/vale_vlan/vv_os_interface.h
+++ sys/dev/vale_vlan/vv_os_interface.h
@@ -17,10 +17,10 @@ void vv_try_module_get(void);
 void vv_module_put(void);
 
 /* DOUBLY LINKED LIST TEMPLATE
- * #define vv_list_declare(head_struct_t, elem_struct_t)
+ * #define vv_list_declare(head_struct_t, entry_struct_t)
  * #define vv_list_head_init(head_p)
- * #define vv_list_elem_init(elem_p)
- * #define vv_list_entry(elem_struct_t)
+ * #define vv_list_elem_init(elem_p, entry_name)
+ * #define vv_list_entry(entry_struct_t)
  * #define vv_list_insert_head(head_p, elem_p, entry_name)
  * #define vv_list_remove(elem_p, entry_name)
  * #define vv_list_foreach(cursor_p, head_p, entry_name)
@@ -28,14 +28,14 @@ void vv_module_put(void);
  */
 
 #if defined(__linux__)
-#define vv_list_declare(head_struct_t, elem_struct_t) struct list_head
+#define vv_list_declare(head_struct_t, entry_struct_t) struct list_head
 
 #define vv_list_head_init(head_p) INIT_LIST_HEAD(head_p)
 
 #define vv_list_elem_init(elem_p, entry_name)                                  \
 	INIT_LIST_HEAD(&(elem_p)->entry_name)
 
-#define vv_list_entry(elem_struct_t) struct list_head
+#define vv_list_entry(entry_struct_t) struct list_head
 
 #define vv_list_insert_head(head_p, elem_p, entry_name)                        \
 	list_add(&(elem_p)->entry_name, head_p)
@@ -49,14 +49,14 @@ void vv_module_put(void);
 	list_for_each_entry_safe(cursor_p, temp_p, head_p, entry_name)
 
 #elif defined(__FreeBSD__)
-#define vv_list_declare(head_struct_t, elem_struct_t)                          \
-	LIST_HEAD(head_struct_t, elem_struct_t)
+#define vv_list_declare(head_struct_t, entry_struct_t)                          \
+	LIST_HEAD(head_struct_t, entry_struct_t)
 
 #define vv_list_head_init(head_p) LIST_INIT(head_p)
 
 #define vv_list_elem_init(elem_p, entry_name) (void)(elem_p)
 
-#define vv_list_entry(elem_struct_t) LIST_ENTRY(elem_struct_t)
+#define vv_list_entry(entry_struct_t) LIST_ENTRY(entry_struct_t)
 
 #define vv_list_insert_head(head_p, elem_p, entry_name)                        \
 	LIST_INSERT_HEAD(head_p, elem_p, entry_name)
